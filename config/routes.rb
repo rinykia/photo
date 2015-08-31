d90bbe213a1618dd08d5e38cb70c68357fbc485c
Natali <rinykia@gmail.com>
2015-08-31 18:58:12 +0300

    Add User

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,13 @@
 Rails.application.routes.draw do
   root 'pages#welcome'
+
+  get 'signup'  => 'users#new' 
+  resources :users
+  get '/profile' => 'users#profile' 
+  get '/login' => 'sessions#new'
+  
+  post 'login' => 'sessions#create'
+  delete 'logout' => 'sessions#destroy'
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
